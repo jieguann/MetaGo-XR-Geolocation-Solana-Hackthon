cfc2e0f70bd85a4a0919a09dfc8fd5050691c86b
jieguann <60665347+jieguann@users.noreply.github.com>
2022-08-16 23:51:46 -0400

    add runtime import

--- unity/Unity-Solana-AR/Assets/Plugins/Piglet/Examples/RuntimeImport/Scripts/RuntimeImportBehaviour.cs
+++ unity/Unity-Solana-AR/Assets/Plugins/Piglet/Examples/RuntimeImport/Scripts/RuntimeImportBehaviour.cs
@@ -17,6 +17,7 @@ public class RuntimeImportBehaviour : MonoBehaviour
     /// </summary>
     private GameObject _model;
     public string modelURL;
+    public float scale;
     /// <summary>
     /// Unity callback that is invoked before the first frame.
     /// Create the glTF import task and set up callbacks for
@@ -44,6 +45,8 @@ public class RuntimeImportBehaviour : MonoBehaviour
     private void OnComplete(GameObject importedModel)
     {
         _model = importedModel;
+        _model.transform.position = transform.position;
+        _model.transform.localScale = new Vector3(scale,scale,scale);
         Debug.Log("Success!");
     }
 
@@ -80,7 +83,6 @@ public class RuntimeImportBehaviour : MonoBehaviour
         _task.MoveNext();
 
         // spin model about y-axis
-        if (_model != null)
-            _model.transform.Rotate(0, 1, 0);
+        
     }
 }
